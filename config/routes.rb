01c5f0d1492e132ae81d5c64cbc118ac37af88a9
kaltag <alexx30rus@gmail.com>
2023-02-22 18:15:10 +0400

    add pagination

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   end
   devise_for :users
 
-  resources :posts
-
+  resources :posts do
+    get '/page/:page', action: :index, on: :collection
+  end
   root 'posts#index'
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Post.destroy_all
+User.destroy_all
+
+user = User.create!(
+  email: 'test@test.com',
+  password: '123456'
+)
+
+(1..20).each do |i|
+  Post.create!(
+    title: "post #{i}",
+    content: 'dsadsa dsa sa dsa dsa dsa dsa dsadasdasd a ',
+    user_id: user.id
+  )
+end
