b3aa4880793fd191aa0f1671d5e3cec9dbb9aa0f
kaltag <alexx30rus@gmail.com>
2023-02-23 12:05:45 +0400

    add image for post

--- Gemfile
+++ Gemfile
@@ -46,7 +46,8 @@ gem "bootsnap", require: false
 # gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+gem 'active_storage_validations'
+gem "image_processing", "~> 1.2"
 
 gem 'cancancan'
 gem 'devise'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,31 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  after_commit :add_default_image, on: %i[update]
+
   paginates_per 15
+
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :content, presence: true, length: { minimum: 20 }
+  validates :image, content_type: ['image/png', 'image/jpeg', 'image/jpg']
+
+  def image_as_thumbnail
+    if image.attached?
+      image
+    else
+      'Spanish.jpg'
+    end
+  end
+
+  private
+
+  def add_default_image
+    return if image.attached?
+
+    image.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'Spanish.jpg')),
+      filename: 'Spanish.jpg',
+      content_type: 'image/jpg'
+    )
+  end
 end
